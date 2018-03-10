d1c06bd9f2c60131e6d1a72f3f64c7f015d29d6e
Dhananjay Balan <dhananjay@cliqz.com>
2018-03-10 14:15:59 +0100

    Use sudo to run in a user.

--- build_package.sh
+++ build_package.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+set -e
 echo 'Building archlinux package for cliqz browser'
 pacman -Syu --noconfirm
-pacman -S git --noconfirm
+pacman -S git sudo --noconfirm
+
+useradd -m -s /bin/bash -d /build build
+echo "build ALL=NOPASSWD: ALL" >> /etc/sudoers
+
 
 git clone https://aur.archlinux.org/package-query.git
 cd package-query
-makepkg -si --noconfirm
+sudo -u build makepkg -si --noconfirm
 cd ..
 git clone https://aur.archlinux.org/yaourt.git
 cd yaourt
-makepkg -si --noconfirm
+sudo -u makepkg -si --noconfirm
 cd ..
