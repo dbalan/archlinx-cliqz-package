16cbbc67a2c052c5bc9a3fc8fb1157427c8441d7
Dhananjay Balan <dhananjay@cliqz.com>
2018-03-10 14:31:05 +0100

    install_devtools

--- setup_system.sh
+++ setup_system.sh
@@ -5,7 +5,7 @@ set -xe
 echo 'Building archlinux package for cliqz browser'
 pacman -Syu --noconfirm
 pacman -S git sudo --noconfirm
-
+pacman -S --needed base-devel --noconfirm
 useradd -m -s /bin/bash build
 echo "build ALL=NOPASSWD: ALL" >> /etc/sudoers
 
